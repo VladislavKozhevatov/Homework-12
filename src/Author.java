3ea4ffa8c53d8351b96fece6f00b49a0d038baaf
Vladislav <tekuzar@mail,ru>
2024-09-26 22:56:52 +0300

    Done!

--- src/Author.java
+++ src/Author.java
@@ -1,16 +1,18 @@
 public class Author {
 
-    private String authorFirstName ;
+    private String authorFirstName;
     private String AuthorSecondName;
 
-    public  Author (String authorFirstName, String AuthorSecondName ){
+    public Author(String authorFirstName, String AuthorSecondName) {
         this.authorFirstName = authorFirstName;
         this.AuthorSecondName = AuthorSecondName;
     }
-    public String getAuthorFirstName(){
+
+    public String getAuthorFirstName() {
         return this.authorFirstName;
     }
-    public  String getAuthorSecondName (){
+
+    public String getAuthorSecondName() {
         return this.AuthorSecondName;
     }
 }
--- src/Book.java
+++ src/Book.java
@@ -1,25 +1,28 @@
 public class Book {
 
-     private String name;
-     private String Author ;
-     private int date;
+    private String name;
+    private String Author;
+    private int date;
 
-     public Book(String name,String Author, int date){
-          this.name = name;
-          this.Author = Author;
-          this.date = date;
-     }
+    public Book(String name, String Author, int date) {
+        this.name = name;
+        this.Author = Author;
+        this.date = date;
+    }
 
-     public String getName(){
-          return this.name;
-     }
-     public  String getAuthor(){
-          return this.Author;
-     }
-     public  int getDatee(){
-          return this.date;
-     }
-     public void setDate(int date){
-          this.date=date;
-     }
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAuthor() {
+        return this.Author;
+    }
+
+    public int getDatee() {
+        return this.date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
 }
